7ac55f4d26885b71cf0cffcd14220adb7153ed27
Haruka <jhht1996@gmail.com>
2022-06-23 11:16:37 +0800

    updates

--- b.py
+++ b.py
@@ -10,7 +10,7 @@ def testfunc(data):
                 ii*jj
 
 def run(niter=10):
-    print(os.sched_getaffinity())
+    os.system("taskset -p 0xff %d" % os.getpid())
     data = (np.random.randn(2,100000) for ii in range(niter))
     pool = Parallel(n_jobs=-1,verbose=1,pre_dispatch='all')
     results = pool(delayed(testfunc)(dd) for dd in data)
